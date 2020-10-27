51fff8d4995fed5bb19704abcdb52f74c5fbc115
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-27 10:22:57 -0700

    added smoke and snapshot tests to App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import { render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// smoke test
+it("renders without crashing", function () {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+// snapshot test
+it("matches snapshot", () => {
+  const {asFragment} = render(<App />)
+  expect(asFragment().toMatchSnapshot)
+})
